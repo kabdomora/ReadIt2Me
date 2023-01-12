6e8f7db3f14d47dc3d031e220e33371a8c980149
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-01-11 23:20:01 -0800

    attempt to generate markdown. buggy needs rework

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -99,11 +101,17 @@ const questions = [
 ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions);
+    inquirer.prompt(questions)
+    .then(userResponses => {
+        writeToFile('README.md', generateMarkdown({...userResponses}), (err) => err ?
+        console.log(err): console.log('Your ReadMe File has been created!'));
+    });
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,37 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.project_title}
+      #### Table of Contents
+      1. [Project Description](#project-description)
+      2. [Installation Instructions](#installation-instructions)
+      3. [How To Use](#how-to-use)
+      4. [How To Contribute](#how-to-contribute)
+      6. [Testing](#testing)
+      7. [License](#license)
+      8. [Contact Me](#contact-me)
 
+      ## Project Description
+      * ${answers.project_description}
+
+      ## Installation Instructions
+      * ${answers.installation}
+
+      ## How To Use
+      * ${answers.purpose}
+
+      ## How To Contribute
+      * ${answers.contribute}
+      
+      ## Testing
+      * ${answers.tests}
+
+      ## License
+      * licensed under the ${answers.license}
+
+      ## Contact Me
+      * Find me on Github: [${answers.github}](http://github.com/${answers.github})
+      * For additional feedback, email me at [${answers.email}](mailto:${answers.email})
 `;
 }
 
